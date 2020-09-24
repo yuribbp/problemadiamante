b463799a532b527168f8db7a633deb8acfeae4ec
Yuri Baldan <yuribbp@gmail.com>
2020-09-24 12:06:38 -0300

    Implementation of interface n ComboDevice class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,26 @@
 package application;
 
-import devices.Printer;
-import devices.Scanner;
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Printer p = new Printer("1080");
+		ConcretePrinter p = new ConcretePrinter("1080");
 		p.processDoc("My Letter");
-		p.printer("My Letter");
+		p.print("My Letter");
 		
-		Scanner s = new Scanner("2003");
+		System.out.println();
+		ConcreteScanner s = new ConcreteScanner("2003");
 		s.processDoc("My e-mail");
 		System.out.println("Scan result: " + s.scan());
+		
+		System.out.println();
+		ComboDevice cd = new ComboDevice("2081");
+		cd.processDoc("My dissertation");
+		cd.print("My dissertation");
+		System.out.println("Scan result " + cd.scan());
 	}
 }
